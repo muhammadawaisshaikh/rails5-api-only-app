dc6d0bdae4930ebb4b14c0454bcb575ba06c5fb2
muhammadawais <awaisshaikh94@gmail.com>
2019-03-24 00:48:04 +0500

    Setting API subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+  end
 end
