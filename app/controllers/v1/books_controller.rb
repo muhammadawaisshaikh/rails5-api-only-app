f96e1a51bd090d91110eebad30530f2aead70af3
muhammadawais <awaisshaikh94@gmail.com>
2019-03-25 03:20:39 +0500

    Action Create - And we will got 201 status response and a json with our new book information, now if you go to your rails c and type Book.last you will get our book created

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 class V1::BooksController < ApplicationController
-    def index
-      books = Book.all
-      render json: { books: books }, status: :ok
+  def index
+    books = Book.all
+    render json: { books: books }, status: :ok
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: { errors: book.errors }, status: :unprocessable_entity
     end
   end
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :author, :weight, :publisher, :language, :pages, :publication_date)
+  end
+
+end
